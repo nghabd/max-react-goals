df7f79c4b3128e891fa03d2849b4801e9d2981be
nghabd <dr_nageh@yahoo.com>
2023-08-23 07:56:17 +0300

    conditional props

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+// import "./CourseInput.css";
 
 const FormControl = styled.div`
 	margin: 0.5rem 0;
@@ -11,30 +11,22 @@ const FormControl = styled.div`
 		font-weight: bold;
 		display: block;
 		margin-bottom: 0.5rem;
+		background: ${(props) => (props.inValid ? "#ffd7d7" : "transparent")};
 	}
 
 	& input {
 		display: block;
 		width: 100%;
-		border: 1px solid #ccc;
+		border: 1px solid ${(props) => (props.inValid ? "red" : "black")};
 		font: inherit;
 		line-height: 1.5rem;
 		padding: 0 0.25rem;
 	}
 
-	& input:focus {
+	& input:hover {
 		outline: none;
 		background: #fad0ec;
 		border-color: #8b005d;
-	}
-
-	&.invalid input {
-		border-color: red;
-		background: #ffd7d7;
-	}
-
-	&.invalid label {
-		color: red;
 	}
 `;
 
@@ -60,7 +52,7 @@ const CourseInput = (props) => {
 
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<FormControl className={!isValid && "invalid"}>
+			<FormControl inValid={!isValid}>
 				<label>Course Goal</label>
 				<input type="text" onChange={goalInputChangeHandler} />
 			</FormControl>
